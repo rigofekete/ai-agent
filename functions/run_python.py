768c395b6351c5620bb493339a199b657040bd09
rigofekete <fabricecosteira@gmail.com>
2026-05-23 19:02:57 +0200

    fix: typo

--- functions/run_python.py
+++ functions/run_python.py
@@ -14,7 +14,7 @@ def run_python_file(working_directory, file_path, args=None):
         return f'Error: File "{file_path}" not found.'
 
     if not file_path.endswith(".py"):
-        return f'Error: "{file_path}" is no a Python file.'
+        return f'Error: "{file_path}" is not a Python file.'
 
     try:
         commands = ["python3", abs_file_path]
